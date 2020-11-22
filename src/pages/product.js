25cd91707c22d558e0d1996431c22b027d5fa931
Rene Tellez Rodriguez <rene.tellez.rodriguez@gmail.com>
2020-11-22 01:43:40 -0800

    Created a couple of pages and setup some links in between them

---
+++ src/pages/product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class product extends Component {
+    render() {
+        return (
+            <div>
+                <h1> this is our products page</h1>
+            </div>
+        )
+    }
+}
